0c31242a921a70341f9cfff3328ae47fcea4b198
Ryan Sun <ryansun81@gmail.com>
2019-06-01 15:42:04 -0500

    my cutting-conor landing page

--- lib/src/landing.dart
+++ lib/src/landing.dart
@@ -7,17 +7,62 @@ class Landing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(30.0), child: Column(
-      children: <Widget>[
-        Text('Vader Wizard'),
-        Text('Intro...'),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: const FlutterLogo(),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          children: <Widget>[
+            Text(
+              'VaderKirk',
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              'A Help Tool for Flutter',
+              style: TextStyle(fontSize: 10),
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
         ),
-      ],
-    ));
+        actions: <Widget>[
+          Row(
+            children: <Widget>[FlutterLogo(), Text('Flutter')],
+          ),
+        ],
+      ),
+      body: Center(
+        child: Padding(padding: EdgeInsets.all(20),child: Column(
+          children: <Widget>[
+            Text('Get widget and community resource help to kickstart '
+                ' your Flutter project faster than a lightsabre launch.',
+                style: TextStyle(fontSize: 20)),
+
+          Padding(padding: EdgeInsets.all(20)),
+
+            Card(
+              child: Container(
+                child: InkWell(child: Padding(padding: EdgeInsets.all(20), child: Column(
+                  children: <Widget>[
+                    Text('Vader Theme', style: TextStyle(fontSize: 40, color: Colors.white),),
+                    Text(
+                        'Quickly customize your flutter widget to match your design vision',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ],
+                ),),
+                onTap: (){
+                  // go to vader
+                  print('vader');
+                },),
+                decoration: BoxDecoration(
+                  color: const Color(0xff7c94b6),
+                  image: DecorationImage(
+                    image: ExactAssetImage('assets/images/vaderrrr.png', scale: 0.02),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),),
+      ),
+    );
   }
 }
